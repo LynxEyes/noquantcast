2e8baed15bb5c9870e072f21d07f069ff2c14d05
LynxEyes <ivo.jesus@gmail.com>
2019-02-07 10:20:56 +0000

    Initial

---
+++ removeQuantcast.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("qc-cmp-ui-showing")[0].className =
+  document.getElementsByClassName("qc-cmp-ui-showing")[0].className.replace("is-truncated is-scrolled is-scrolled-below-header qc-cmp-ui-showing", "");
+document.getElementsByClassName("qc-cmp-ui-container")[0].remove();
